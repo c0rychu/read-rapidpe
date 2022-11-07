0e7a10d07fe7496e5a3d0105bb180fb1f7b9be1c
Cory Chu <cory@gwlab.page>
2022-11-07 03:56:27 -0600

    change interface to correct names

--- read_rapidpe/parser.py
+++ read_rapidpe/parser.py
@@ -1,5 +1,6 @@
 from ligo.lw import utils, lsctables, ligolw
 
+
 class RapidPE_XML:
     """
     Parse the RapidPE result ligo-lw XML files
@@ -13,20 +14,56 @@ class RapidPE_XML:
 
     intrinsic_table : dict
         Intrinsic parameters get from "sngl_inspiral:table"
-        
+
     extrinsic_table : dict
         Extrinsic parameters get from "sim_inspiral:table"
 
     """
     def __init__(self, filename: str):
         self.xmldoc = self._open_ligolw(filename)
-        self.intrinsic_table = self._get_ligolw_table(lsctables.SnglInspiralTable)
+        self.intrinsic_table_raw = self._get_ligolw_table(
+            lsctables.SnglInspiralTable
+            )
         try:
-            self.extrinsic_table = self._get_ligolw_table(lsctables.SimInspiralTable)
-        except ValueError as e:
+            self.extrinsic_table_raw = self._get_ligolw_table(
+                lsctables.SimInspiralTable
+                )
+        except ValueError:
             # if not *samples.xml.gz, there is no "sim_inspiral:table"
-            self.extrinsic_table = {}
-    
+            self.extrinsic_table_raw = {}
+            pass
+
+        self.intrinsic_table = {}
+        try:
+            self.intrinsic_table = {
+                "mass_1": self.intrinsic_table_raw["mass1"],
+                "mass_2": self.intrinsic_table_raw["mass2"],
+                "marg_ln_likelihood": self.intrinsic_table_raw["snr"],
+                "spin1z": self.intrinsic_table_raw["spin1z"],
+                "spin2z": self.intrinsic_table_raw["spin2z"],
+                "tau0": self.intrinsic_table_raw["tau0"],
+                "tau3": self.intrinsic_table_raw["tau3"]
+            }
+        except KeyError:
+            pass
+
+        self.extrinsic_table = {}
+        try:
+            self.extrinsic_table = {
+                "mass_1": self.extrinsic_table_raw["mass1"],
+                "mass_2": self.extrinsic_table_raw["mass2"],
+                "distance": self.extrinsic_table_raw["distance"],
+                "latitude": self.extrinsic_table_raw["latitude"],
+                "longitude": self.extrinsic_table_raw["longitude"],
+                "inclination": self.extrinsic_table_raw["inclination"],
+                "polarization": self.extrinsic_table_raw["polarization"],
+                "ln_likelihood": self.extrinsic_table_raw["alpha1"],
+                "prior": self.extrinsic_table_raw["alpha2"],
+                "sampling_function": self.extrinsic_table_raw["alpha3"],
+                }
+        except KeyError:
+            pass
+
     def _open_ligolw(self, xmlfile: str):
         xmldoc = utils.load_filename(
             xmlfile, contenthandler=ligolw.LIGOLWContentHandler
@@ -35,8 +72,8 @@ class RapidPE_XML:
 
     def _get_ligolw_table(self, lsctable):
         xmltable = lsctable.get_table(self.xmldoc)
-        table = { key: [] for key in xmltable.columnnames} 
+        table = {key: [] for key in xmltable.columnnames}
         for row in xmltable:
             for key in table.keys():
                 table[key].append(getattr(row, key))
-        return table   
+        return table
