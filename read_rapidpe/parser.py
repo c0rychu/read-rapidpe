e64bbca365bcc529c1e3119387306f5ce5b7c11d
Cory Chu <cory@gwlab.page>
2022-11-07 23:31:55 -0600

    update api and comments

--- read_rapidpe/parser.py
+++ read_rapidpe/parser.py
@@ -9,9 +9,11 @@ class RapidPE_XML:
 
     Attributes
     ----------
+    TODO: Complete the example/explanation of parameters
     intrinsic_table : dict
         Intrinsic parameters get from "sngl_inspiral:table"
-        with canonical parameter names
+        with canonical parameter names.
+        An example:
         {
             "mass_1": [1.4],
             "mass_2": [2.8],
@@ -56,22 +58,25 @@ class RapidPE_XML:
         # Provide canonical interface with two attributes
         #   self.intrinsic_table
         #   self.extrinsic_table
-        # TODO: Check the naming convention in bilby
-        #       https://arxiv.org/pdf/2006.00714.pdf
+        # TODO: Check the naming convention in bilby and PESummary
+        #       [bilby]    https://arxiv.org/pdf/2006.00714.pdf
+        #       [PESummary]https://docs.ligo.org/lscsoft/pesummary/stable_docs/gw/parameters.html
         self.intrinsic_table = {}
         try:
             self.intrinsic_table = {
                 "mass_1": self.intrinsic_table_raw["mass1"],
                 "mass_2": self.intrinsic_table_raw["mass2"],
-                "marg_ln_likelihood": self.intrinsic_table_raw["snr"],
-                "spin1z": self.intrinsic_table_raw["spin1z"],
-                # spin1z = chi_1 = spin_1_z ?
-                "spin2z": self.intrinsic_table_raw["spin2z"],
-                # spin2z = chi_2 = spin_2_z ?
+                "marg_log_likelihood": self.intrinsic_table_raw["snr"],
+                "spin_1z": self.intrinsic_table_raw["spin1z"],
+                # spin1z = chi_1(bilby) = spin_1_z(bilby) = spin_1z(PESummary)?
+                "spin_2z": self.intrinsic_table_raw["spin2z"],
+                # spin2z = chi_2(bilby) = spin_2_z(bilby) = spin_2z(PESummary)?
                 "tau0": self.intrinsic_table_raw["tau0"],
-                # what is tau0 ?
-                "tau3": self.intrinsic_table_raw["tau3"]
-                # what is tau3 ?
+                "tau3": self.intrinsic_table_raw["tau3"],
+                # (tau0, tau3) are called chirptime parameters
+                # It's a kind of mass parameter
+                # like (m1, m2) or (chirp_mass, mass_ratio)
+                # See Appendix B of https://arxiv.org/pdf/0706.4437.pdf
             }
         except KeyError:
             pass
@@ -81,20 +86,21 @@ class RapidPE_XML:
             self.extrinsic_table = {
                 "mass_1": self.extrinsic_table_raw["mass1"],
                 "mass_2": self.extrinsic_table_raw["mass2"],
-                "distance": self.extrinsic_table_raw["distance"],
+                "luminosity_distance": self.extrinsic_table_raw["distance"],
                 # distance = luminosity_distance?
                 "latitude": self.extrinsic_table_raw["latitude"],
                 # latitude = dec ?
                 "longitude": self.extrinsic_table_raw["longitude"],
                 # longitude = ra ?
-                "inclination": self.extrinsic_table_raw["inclination"],
+                "theta_jn": self.extrinsic_table_raw["inclination"],
                 # inclination = theta_jn ?
-                "polarization": self.extrinsic_table_raw["polarization"],
+                "psi": self.extrinsic_table_raw["polarization"],
                 # polarization = psi?
-                "ln_likelihood": self.extrinsic_table_raw["alpha1"],
+                "log_likelihood": self.extrinsic_table_raw["alpha1"],
                 "prior": self.extrinsic_table_raw["alpha2"],
                 "sampling_function": self.extrinsic_table_raw["alpha3"],
-                # what is sampling_function ?
+                # sampling_function is
+                # the p_s in Eq. 28 of https://arxiv.org/pdf/1502.04370.pdf
                 }
         except KeyError:
             pass
