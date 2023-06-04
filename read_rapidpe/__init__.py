64ce129ddd449e590098f1e5be2120d9325ea5eb
Cory Chu <cory@gwlab.page>
2023-06-04 14:19:06 -0500

    add pretty_plot

--- read_rapidpe/__init__.py
+++ read_rapidpe/__init__.py
@@ -1,11 +1,12 @@
 from .grid_point import RapidPE_grid_point
 from .result import RapidPE_result
 from .p_astro import p_astro
-from .plot import pretty_plt
+from .plot import pretty_plt, pretty_plot
 from .transform import Mass_Spin
 
 __all__ = ["RapidPE_grid_point",
            "RapidPE_result",
            "Mass_Spin",
            "p_astro",
-           "pretty_plt"]
+           "pretty_plt",
+           "pretty_plot"]
--- read_rapidpe/plot.py
+++ read_rapidpe/plot.py
@@ -1,12 +1,17 @@
 import numpy as np
+from contextlib import contextmanager
 from .transform import Mass_Spin
 
 
 def pretty_plt(plt, usetex=True):
     """
-    import read_rapidpe.plot as rrp
-    import matplotlib.pyplot as plt
-    rrp.pretty_plt(plt)
+    Usage:
+        import read_rapidpe.plot as rrp
+        import matplotlib.pyplot as plt
+        rrp.pretty_plt(plt)
+
+    It can be reset by:
+        plt.rcdefaults()
     """
     # -----------------------------------------------
     # Reference:
@@ -31,6 +36,60 @@ def pretty_plt(plt, usetex=True):
     plt.rc('font', family='serif')
     plt.rc('lines', linewidth=0.5)  # Linewidth of data
     plt.rc('savefig', dpi=300)
+
+
+@contextmanager
+def pretty_plot(rc={}, use_preset=True):
+    """
+    Context manager for pretty plots
+    Usage:
+        with pretty_plot():
+            plt.plot(x, y)
+            ...
+        with pretty_plot(rc={"lines.linewidth": 0.2}):
+            plt.plot(x, y)
+            ...
+
+    Parameters
+    ----------
+    rc : dict
+        Dictionary of matplotlib rc parameters
+
+    use_preset : bool
+        Whether to use preset pretty rc parameters
+
+    """
+    import matplotlib.pyplot as plt
+    if use_preset:
+        rc_params = {
+                "text.usetex": True,
+                "font.family": "serif",
+                "axes.labelsize": "medium",
+                "axes.linewidth": 0.5,
+                "axes.labelpad": 4,
+                "xtick.major.width": 0.5,
+                "xtick.major.pad": 7,
+                "ytick.major.width": 0.5,
+                "ytick.major.pad": 4,
+                "xtick.minor.width": 0.5,
+                "ytick.minor.width": 0.5,
+                "xtick.labelsize": "small",
+                "xtick.direction": "in",
+                "xtick.top": True,
+                "ytick.labelsize": "small",
+                "ytick.direction": "in",
+                "ytick.right": True,
+                "grid.linewidth": 0.5,
+                "grid.color": "0.9",
+                "lines.linewidth": 0.5,
+                "savefig.dpi": 300,
+            }
+    else:
+        rc_params = {}
+    rc_params.update(rc)
+
+    with plt.rc_context(rc=rc_params) as rc_context:
+        yield rc_context
 
 
 def meshgrid_mceta(result, n=100):
@@ -77,7 +136,7 @@ def plot_corner(samples, show_titles=True, **kwargs):
 
 
 def plot_grid(res, posterior_samples=True):
-    from matplotlib import pyplot as plt
+    import matplotlib.pyplot as plt
     if posterior_samples:
         try:
             m1 = res.posterior_samples["mass_1"]
